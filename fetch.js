e28aeb47f438fc66533bc5c63a20ada4aaa7c3b2
rivermanvr <vrgithub@riversconsulting.com>
2018-08-31 15:41:18 -0400

    reseting the repo....

--- fetch.js
+++ fetch.js
@@ -1,10 +1,4 @@
-fetch('./template.html')
+fetch('https://api.github.com/users/google/repos')
   .then(function (response) {
-
-      return response.text()
-  }).then(function(html) {
-        document.getElementById('body').innerHTML = html;
-  }).catch(function(error) {
-    console.log('Fetch error');
-    console.log(error);
-  });
+    console.log(response);
+  })
